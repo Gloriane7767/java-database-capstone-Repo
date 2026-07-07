3b0360dcfb0effa7952a4392c1aef49a98ecd170
Gloriane7767 <gloriane7767@gmail.com>
2026-07-07 17:02:38 -0400

    Fix appointmentRow.js field names and undefined variable bug

--- app/src/main/resources/static/js/components/appointmentRow.js
+++ app/src/main/resources/static/js/components/appointmentRow.js
@@ -1,19 +1,15 @@
-// appointmentRow.js
+
 export function getAppointments(appointment) {
   const tr = document.createElement("tr");
-
   tr.innerHTML = `
       <td class="patient-id">${appointment.patientName}</td>
       <td>${appointment.doctorName}</td>
-      <td>${appointment.date}</td>
-      <td>${appointment.time}</td>
+      <td>${appointment.appointmentDate}</td>
+      <td>${appointment.appointmentTimeOnly}</td>
       <td><img src="../assets/images/edit/edit.png" alt="action" class="prescription-btn" data-id="${appointment.id}"></img></td>
     `;
-
-  // Attach event listeners
   tr.querySelector(".prescription-btn").addEventListener("click", () => {
-    window.location.href = `addPrescription.html?id=${patient.id}`;
+    window.location.href = `/pages/addPrescription.html?appointmentId=${appointment.id}&patientName=${appointment.patientName}`;
   });
-
   return tr;
 }
